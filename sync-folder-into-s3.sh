72ee81bce4eb0529d49d821b9a345a5292af13a4
Antonio Feijao UK <2548084+AntonioFeijaoUK@users.noreply.github.com>
2020-07-01 10:38:23 +0100

    Create sync-folder-into-s3.sh

---
+++ sync-folder-into-s3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#aws s3 sync /home/ec2-user/pastebin/s3-pastebin-macie-demo/ s3://s3-pastebin-macie-demo/ --delete
+
+
+aws s3 sync /home/ec2-user/pastebin/s3-pastebin-macie-demo/ s3://s3-pastebin-macie-demo/
